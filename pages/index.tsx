fe193ab4123c12ae22536fb8cb0080841df831df
sujathareddy <sujathreddypallerla24@gmail.com>
2023-07-06 12:32:07 +0530

    final

---
+++ pages/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import InfoModal from "@/components/InfoModal";
+import useMovieList from "@/hooks/useMovieList";
+import useFavoties from "@/hooks/useFavorites";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
+
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+// export default function Home() {
+//   return (
+//   <>
+//   <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+//   <button className="h-10 w-full bg-white" onClick={() => signOut()}>LogOut</button>
+//   </>
+//   )
+// }
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore();
+
+  return (
+    <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
